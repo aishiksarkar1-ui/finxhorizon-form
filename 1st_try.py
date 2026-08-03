3340d1e23610c22f0ead00dc1a6852aed231b603
aishiksarkar1-ui <aishiksarkar1@gmail.com>
2026-08-03 14:00:16 +0530

    Update 1st_try.py

--- 1st_try.py
+++ 1st_try.py
@@ -143,7 +143,7 @@ elif st.session_state.page == 'main_form':
 
 
                 # স্ক্রিনশটের মতো কোডের মাধ্যমে সরাসরি পরের ট্যাবে যাওয়ার জন্য
-                st.session_state.current_tab_index = "1"
+                st.session_state.current_tab_index = 1
                 st.rerun()
                 
             else:
